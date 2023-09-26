822ad5284e695c6318a0802ae33624e323f20438
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-09-26 10:37:33 +0200

    Assignement Completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      // Reset timeRemaining to 10 seconds for the next question
+      setTimeRemaining(10);
+      // Trigger onAnswered with a value of false
+      onAnswered(false);
+    } else {
+     
+      const timerId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+     
+      return () => clearTimeout(timerId);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
